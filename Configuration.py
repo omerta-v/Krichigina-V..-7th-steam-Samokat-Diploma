bd0af363cc2e99f0925c9680c6da83efb570d3fd
omerta-v <omerta.v@yandex.ru>
2023-08-02 12:20:41 +0300

    First Commit
    First Commit

---
+++ Configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://27448fca-edc9-4b9b-918a-49e179f5b1f9.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+CREATE_ORDER="/api/v1/orders"
+GET_ORDER="/api/v1/orders/track?t="
---
+++ Order.py
@@ -0,0 +1,46 @@
+import requests
+import Data
+import Configuration
+
+
+#Creating new Order for track
+def post_new_order(body):
+    return requests.post(Configuration.URL_SERVICE + Configuration.CREATE_ORDER,
+                         json=body,
+                         headers=Data.headers)  # а здесь заголовки
+
+#Storing track into variable for convience
+track = post_new_order(Data.order_body).json()["track"]
+
+
+
+#Creating function for getting order data by track
+def get_order_data(tracknumber):
+    return requests.get(Configuration.URL_SERVICE + Configuration.GET_ORDER + str(tracknumber))
+
+#Printing some values for cohesive debugging, comment them if not needed
+print (track)
+print (get_order_data(track))
+print (get_order_data(""))
+
+#Check Response Code for getting order Data with tracknumber
+def positive_assert(trackorder):
+    response = get_order_data(trackorder)
+    assert response.status_code == 200
+
+#Check Response Code for getting order Data without tracknumber
+def negative_assert(trackorder):
+    response = get_order_data(trackorder)
+    assert response.status_code == 400
+    assert response.json()["message"] == "Недостаточно данных для поиска"
+
+#Tests:
+
+#Checking response status code for order with track
+def test_order_data_received():
+    positive_assert(track)
+
+#Checking response status code for order without track
+def test_order_data_received_without_track():
+    track=""
+    negative_assert(track)
